7ed3edf26afca2f3e23ddbedbe13d8aa2424645b
abisara2k1 <abisara@iu.edu>
2025-02-25 18:27:05 -0500

    Completed till 46th slide

---
+++ app/src/main/java/com/example/lab8/PhotoRepository.kt
@@ -0,0 +1,22 @@
+package com.example.lab8
+
+import com.example.lab8.network.Flicker
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
+
+class PhotoRepository {
+
+    private val flicker: Flicker
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+
+        flicker = retrofit.create<Flicker>()
+    }
+
+    suspend fun fetchContents() = flicker.fetchContents()
+}
---
+++ app/src/main/java/com/example/lab8/network/Flicker.kt
@@ -0,0 +1,9 @@
+package com.example.lab8.network
+
+import retrofit2.http.GET
+
+
+interface Flicker {
+    @GET("/")
+    suspend fun fetchContents(): String
+}
